60537262bd9682e948910a7d5e3ff07f2f6439ad
Nbryan11 <108432413+Nbryan11@users.noreply.github.com>
2025-06-23 18:57:18 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
           <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
           
             <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white">
-              El Reino de Dios
+              Poli Ecommerce
             </span>
           </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -28,7 +28,7 @@ function Footer() {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2025 <a href="/" className="hover:underline font-semibold">El Reino de Dios™</a>. Todos los derechos reservados.
+          © 2025 <a href="/" className="hover:underline font-semibold">Poli Ecommerce™</a>. Todos los derechos reservados.
         </span>
       </div>
     </footer>
